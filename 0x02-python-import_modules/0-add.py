1577e8f9543697f4d772dfa9603d9c2d0c014566
Sg1787 <sgrobbler1@gmail.com>
2024-01-05 04:48:05 -0800

    task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+if_name_=="_main_":
+    from add _0 import add
+
+    a=1
+    b=2
+
+    print(a + b)
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,4 @@
+all of the first lines in all of the files should be as follows: 
+#!/usr/bin/python3
+
+Good luck 
